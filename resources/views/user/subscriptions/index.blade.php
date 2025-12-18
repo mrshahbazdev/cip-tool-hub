7ecc24f3957ad00466236cc9f63956ddf264800d
unknown <mr.shahbaznns@gmail.com>
2025-12-18 14:33:16 +0500

    forget page change

--- resources/views/user/subscriptions/index.blade.php
+++ resources/views/user/subscriptions/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <x-slot name="header">
+    <x-slot name="header" class="bg-white dark:bg-gray-800 shadow-sm border-b border-gray-200 dark:border-gray-700">
         <div class="bg-white dark:bg-gray-800 shadow-sm border-b border-gray-200 dark:border-gray-700">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
                 <div class="flex items-center justify-between">
